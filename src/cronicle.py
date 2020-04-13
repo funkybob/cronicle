ac4f18ed25379b07154dfe15e9cefda34db0be3d
Curtis Maloney <curtis@tinbrain.net>
2020-04-13 16:51:22 +1000

    Dry why/matches. Clean with flake8+mypy

--- src/cronicle.py
+++ src/cronicle.py
@@ -2,46 +2,42 @@ import re
 
 from collections import namedtuple
 from datetime import datetime, tzinfo, timezone
+from typing import Tuple
 
-Pattern = namedtuple('Pattern', 'minute hour dom month dow')
+Pattern = namedtuple("Pattern", "minute hour dom month dow")
 
-slash_re = re.compile('^\*/(\d+)$')
+slash_re = re.compile(r"^\*/(\d+)$")
 
 
 class Cron:
     def __init__(self, pattern: str, timezone: tzinfo = timezone.utc):
         self.tz = timezone
-        self.pattern = Pattern(*pattern.split(' '))
+        self.pattern = Pattern(*pattern.split(" "))
 
-    def matches(self, when: datetime):
-        _when = when.astimezone(self.tz)
-        return all([
-            self.match_minute(_when),
-            self.match_hour(_when),
-            self.match_dom(_when),
-            self.match_month(_when),
-            self.match_dow(_when),
-        ])
+    def matches(self, when: datetime) -> bool:
+        """
+        Tests if this pattern matches the given datetime.
+        """
+        return all(self.why(when))
 
-    def why(self, when: datetime):
+    def why(self, when: datetime) -> Tuple[bool, bool, bool, bool, bool]:
         """
         Explains why a pattern matches a datetime.
         """
         _when = when.astimezone(self.tz)
-        return [
+        return (
             self.match_minute(_when),
             self.match_hour(_when),
             self.match_dom(_when),
             self.match_month(_when),
             self.match_dow(_when),
-        ]
+        )
 
-    @staticmethod
-    def _match(frag: str, value: int):
-        '''
+    def _match(self, frag: str, value: int):
+        """
         Helper to match pattern fragment with date value
-        '''
-        if frag == '*':
+        """
+        if frag == "*":
             return True
 
         m = slash_re.search(frag)
@@ -49,11 +45,11 @@ class Cron:
             return value % int(m.group(1)) == 0
 
         try:
-            frat = int(frag)
+            val = int(frag)
         except ValueError:
-            raise ValueError('Invalid pattern: %s' % self.pattern)
+            raise ValueError("Invalid pattern: %s" % (self.pattern,))
 
-        return int(frag) == value
+        return val == value
 
     def match_minute(self, when: datetime) -> bool:
         return self._match(self.pattern.minute, when.minute)
@@ -69,4 +65,3 @@ class Cron:
 
     def match_dow(self, when: datetime) -> bool:
         return self._match(self.pattern.dow, when.weekday())
-
