af03bd3d896b6fc31cfbe8a7322e512958013080
Curtis Maloney <curtis@tinbrain.net>
2020-04-13 18:45:44 +1000

    Pre-parse patterns

--- src/cronicle.py
+++ src/cronicle.py
@@ -2,18 +2,69 @@ import re
 
 from collections import namedtuple
 from datetime import datetime, tzinfo, timezone
+from functools import partial
 from typing import Tuple
 
-Pattern = namedtuple("Pattern", "minute hour dom month dow")
+FIELDS = ["minute", "hour", "dom", "month", "dow"]
 
 slash_re = re.compile(r"^\*/(\d+)$")
 list_re = re.compile(r"(\d+)(?:,(\d+))+$")
+
+def match_splot(value):
+    return True
+
+def match_slash(value, *, divisor):
+    return value % divisor == 0
+
+def match_list(value, *, values):
+    return value in values
+
+def match_value(value, *, match):
+    return value == match
+
+
+GETTER = {
+    'minute': lambda when: when.minute,
+    'hour': lambda when: when.hour,
+    'dom': lambda when: when.day,
+    'month': lambda when: when.month,
+    'dow': lambda when: when.weekday(),
+}
 
 
 class Cron:
     def __init__(self, pattern: str, timezone: tzinfo = timezone.utc):
         self.tz = timezone
-        self.pattern = Pattern(*pattern.split(" "))
+        self.pattern = pattern
+
+        frags = pattern.split(' ')
+        if len(frags) != len(FIELDS):
+            raise ValueError('Invalid pattern: wrong number of fields.')
+
+        # A map of {field: match} functions per field
+        self.tester = {
+            field: self._get_tester(frag)
+            for field, frag in zip(FIELDS, frags)
+        }
+
+    def _get_tester(self, frag: str):
+        if frag == '*':
+            return match_splot
+
+        m = slash_re.search(frag)
+        if m:
+            return partial(match_slash, divisor=int(m.group(1)))
+
+        m = list_re.search(frag)
+        if m:
+            return partial(match_list, values=[int(x) for x in frag.split(",")])
+
+        try:
+            val = int(frag)
+        except ValueError:
+            raise ValueError("Invalid pattern: %s is not a number" % (frag,))
+
+        return partial(match_value, match=val)
 
     def matches(self, when: datetime) -> bool:
         """
@@ -26,47 +77,11 @@ class Cron:
         Explains why a pattern matches a datetime.
         """
         _when = when.astimezone(self.tz)
-        return (
-            self.match_minute(_when),
-            self.match_hour(_when),
-            self.match_dom(_when),
-            self.match_month(_when),
-            self.match_dow(_when),
+        print({
+            field: GETTER[field](when)
+            for field in FIELDS
+        })
+        return tuple(
+            self.tester[field](GETTER[field](when))
+            for field in FIELDS
         )
-
-    def _match(self, frag: str, value: int):
-        """
-        Helper to match pattern fragment with date value
-        """
-        if frag == "*":
-            return True
-
-        m = slash_re.search(frag)
-        if m:
-            return value % int(m.group(1)) == 0
-
-        m = list_re.search(frag)
-        if m:
-            return value in [int(x) for x in frag.split(",")]
-
-        try:
-            val = int(frag)
-        except ValueError:
-            raise ValueError("Invalid pattern: %s" % (self.pattern,))
-
-        return val == value
-
-    def match_minute(self, when: datetime) -> bool:
-        return self._match(self.pattern.minute, when.minute)
-
-    def match_hour(self, when: datetime) -> bool:
-        return self._match(self.pattern.hour, when.hour)
-
-    def match_dom(self, when: datetime) -> bool:
-        return self._match(self.pattern.dom, when.day)
-
-    def match_month(self, when: datetime) -> bool:
-        return self._match(self.pattern.month, when.month)
-
-    def match_dow(self, when: datetime) -> bool:
-        return self._match(self.pattern.dow, when.weekday())
