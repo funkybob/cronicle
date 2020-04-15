47af8ba7ddd5b6ea059fcbe42eb6a7d47d597ce3
Curtis Maloney <curtis@tinbrain.net>
2020-04-15 16:53:49 +1000

    [Fixes #1] Add test for Not-Monday matching 0

--- test/test_cronicle.py
+++ test/test_cronicle.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 import pytest
 
@@ -22,33 +22,36 @@ def test_matches(pattern, when, match):
 
 @pytest.mark.parametrize('pattern, match', [
     ('* * * * *', (True, True, True, True, True)),
-
+    # Minute
     ('1 * * * *', (False, True, True, True, True)),
     ('*/4 * * * *', (False, True, True, True, True)),
     ('1,4,59 * * * *', (False, True, True, True, True)),
     ('1-14 * * * *', (False, True, True, True, True)),
-
+    # Hour
     ('* 3 * * *', (True, False, True, True, True)),
     ('* */5 * * *', (True, False, True, True, True)),
     ('* 1,5,20 * * *', (True, False, True, True, True)),
     ('* 1-10 * * *', (True, False, True, True, True)),
-
+    # DoM
     ('* * 12 * *', (True, True, False, True, True)),
     ('* * */4 * *', (True, True, False, True, True)),
     ('* * 1,12,24 * *', (True, True, False, True, True)),
     ('* * 1-12 * *', (True, True, False, True, True)),
-
+    # Month
     ('* * * 2 *', (True, True, True, False, True)),
     ('* * * */3 *', (True, True, True, False, True)),
     ('* * * 1,3,11 *', (True, True, True, False, True)),
     ('* * * 5-11 *', (True, True, True, False, True)),
-
+    # DoW
     ('* * * * 2', (True, True, True, True, False)),
     ('* * * * */2', (True, True, True, True, False)),
     ('* * * * 2,5', (True, True, True, True, False)),
     ('* * * * 2-5', (True, True, True, True, False)),
+    # 0 == Monday
+    ('* * * * 0', (True, True, True, True, False)),
 ])
 def test_why(pattern, match):
-    when = datetime(2020, 4, 14, 11, 15)
+    # 'Tue Apr 14 11:15:00 2020'
+    when = datetime(2020, 4, 14, 11, 15, tzinfo=timezone.utc)
 
     assert Cron(pattern).why(when) == match
